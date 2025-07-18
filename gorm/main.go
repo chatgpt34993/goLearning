2fc28c1daf0ebd474232ac1af859b5ae4ca6caa8
wangtenghui <850423071@qq.com>
2025-07-18 18:30:33 +0800

    任务2

---
+++ gorm/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"myGorm/lesson01"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+
+	db, err := gorm.Open(mysql.Open("root:11223344@tcp(127.0.0.1:3306)/gorm?charset=utf8mb4&parseTime=True&loc=Local"))
+	if err != nil {
+		panic(err)
+	}
+
+	//InitDB(&Parent{}, &Child{})
+	lesson01.Run(db)
+}
